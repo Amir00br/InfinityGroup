7865166717869f7db5aa3e9f9d2237a7d98eb8a6
Amir <infiniitygroup2023@gmail.com>
2023-06-19 18:17:25 +0330

    insert into

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // هرگونه عملکردی که برای وبسایت نیاز دارید را در اینجا اضافه کنید
 // (مثلاً اعتبارسنجی فرم، تعامل و غیره)
+document.getElementById("group-title").addEventListener("click", function() {
+    window.location.href ="index.html";
+});
 
